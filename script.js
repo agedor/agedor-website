367bbe89d417eaca2ffb445642c299f61375d017
agedor <info@agedor-inc.com>
2026-09-21 16:10:31 +0900

    ko

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const menu=document.querySelector('.menu');const nav=document.querySelector('.header nav');menu.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+const year = document.getElementById("year");
+
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
